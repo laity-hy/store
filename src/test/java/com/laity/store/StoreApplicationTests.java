bfe7a9f3cd3edd8d486893ab272f113c18b4b0db
laity <l1810638451@gmail.com>
2022-11-20 17:28:38 +0800

    Initial commit

--- src/test/java/com/laity/store/StoreApplicationTests.java
+++ src/test/java/com/laity/store/StoreApplicationTests.java
@@ -1,13 +1,29 @@
 package com.laity.store;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class StoreApplicationTests {
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     void contextLoads() {
+    }
+
+    /**
+     * 测试数据库连接是否正确
+     *
+     * @throws SQLException
+     */
+    @Test
+    void getConnection() throws SQLException {
+        System.out.println(dataSource.getConnection());
     }
 
 }
