4af2b7a7bffe58ff7f1780217064f1bfb2c6ff6b
laity <l1810638451@gmail.com>
2022-11-21 16:14:20 +0800

    Initial commit

---
+++ src/main/java/com/laity/store/service/impl/IUserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.laity.store.service.impl;
+
+import com.laity.store.entity.User;
+import com.laity.store.service.IUserService;
+
+/**
+ * 用户模块业务层的实现类
+ */
+public class IUserServiceImpl implements IUserService {
+    @Override
+    public void reg(User user) {
+
+    }
+}
